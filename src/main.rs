5a1ec741371deb931eb06c7dcfc1d40a03e1ee8e
Matt Fehskens <matt.fehskens@gmail.com>
2018-03-15 22:00:47 -0400

    Reading requests to string

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!(
+        "Request: {}",
+        String::from_utf8_lossy(&buffer[..]),
+    );
 }
