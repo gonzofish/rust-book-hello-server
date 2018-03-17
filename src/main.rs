8fc67144a6c7b5aecedf23b3431ef622705e876e
Matt Fehskens <matt.fehskens@gmail.com>
2018-03-16 23:42:03 -0400

    Creating a (homegrown) ThreadPool

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+extern crate hello;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
