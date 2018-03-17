6b4d2843fd236bc409646cf19c92497c4b95ff86
Matt Fehskens <matt.fehskens@gmail.com>
2018-03-17 14:59:20 -0400

    Workers execute threads via channel & a new `FnBox` trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,49 @@
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
+
+trait FnBox {
+  fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+  fn call_box(self: Box<F>) {
+    (*self)()
+  }
+}
+
+type Job = Box<FnBox + Send + 'static>;
 
 pub struct ThreadPool {
   workers: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
 
+    let (sender, plain_receiver) = mpsc::channel();
+    let receiver = Arc::new(Mutex::new(plain_receiver));
     let mut workers = Vec::with_capacity(size);
 
     for id in 0 .. size {
-      workers.push(Worker::new(id));
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
 
     ThreadPool {
-      workers
+      workers,
+      sender,
     }
   }
 
   pub fn execute<F>(&self, f: F)
     where F: FnOnce() + Send + 'static
   {
+    let job = Box::new(f);
 
+    self.sender
+      .send(job)
+      .unwrap();
   }
 }
 
@@ -32,8 +53,19 @@ struct Worker {
 }
 
 impl Worker {
-  pub fn new(id: usize) -> Worker {
-    let thread = thread::spawn(|| {});
+  pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    let thread = thread::spawn(move || {
+      loop {
+        let job = receiver.lock() // get the Receiver<Job>
+          .unwrap()
+          .recv() // Receive the message
+          .unwrap();
+
+        println!("Worker {} got a job! Executing...", id);
+
+        job.call_box();
+      };
+    });
 
     Worker {
       id,
