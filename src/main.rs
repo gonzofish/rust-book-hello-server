0664d0a4c9618e292ae89207a76fa00ec37605b0
Matt Fehskens <matt.fehskens@gmail.com>
2018-03-16 23:14:33 -0400

    Returning an actual HTML file!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
@@ -16,7 +17,12 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("index.html").unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
